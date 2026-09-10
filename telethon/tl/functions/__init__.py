ebca50faea5469baff6125c1ccf2a971528787b6
hadish0123 <hatf9073@gmail.com>
2026-09-10 22:28:46 +0330

    Remove incomplete Telethon functions shim

--- telethon/tl/functions/__init__.py
+++
@@ -1,9 +0,0 @@
-class _Request:
-    def __init__(self, *args, **kwargs):
-        self.__dict__.update(kwargs)
-        self.args = args
-
-def __getattr__(name):
-    cls = type(name, (_Request,), {})
-    globals()[name] = cls
-    return cls
