44d6ccdc51730513f57caefa3a1f4ebed1b25918
hadish0123 <hatf9073@gmail.com>
2026-09-10 21:40:02 +0330

    Expand Bot API event filter compatibility

--- telethon/events.py
+++ telethon/events.py
@@ -8,14 +8,39 @@ class StopPropagation(Exception):
 
 
 class _Builder:
-    def __init__(self, *args, incoming=None, func=None, pattern=None, data=None, **kwargs):
+    def __init__(self, *args, incoming=None, outgoing=None, func=None, pattern=None, data=None, chats=None,
+                 from_users=None, blacklist_chats=False, **kwargs):
         self.incoming = incoming
+        self.outgoing = outgoing
         self.func = func
         self.pattern = pattern
         self.data = data
+        self.chats = chats
+        self.from_users = from_users
+        self.blacklist_chats = blacklist_chats
+
+    @staticmethod
+    def _ids(value):
+        if value is None:
+            return set()
+        if isinstance(value, (list, tuple, set, frozenset)):
+            return {getattr(v, "id", v) for v in value}
+        return {getattr(value, "id", value)}
 
     async def matches(self, event):
         if self.incoming is not None and getattr(event, "incoming", True) != self.incoming:
+            return False
+        if self.outgoing is not None and getattr(event, "out", False) != self.outgoing:
+            return False
+        if self.chats is not None:
+            chat_id = getattr(event, "chat_id", None)
+            in_chats = chat_id in self._ids(self.chats)
+            if self.blacklist_chats:
+                if in_chats:
+                    return False
+            elif not in_chats:
+                return False
+        if self.from_users is not None and getattr(event, "sender_id", None) not in self._ids(self.from_users):
             return False
         if self.pattern is not None:
             text = getattr(event, "raw_text", None) or getattr(event, "text", "") or ""
